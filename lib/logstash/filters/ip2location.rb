1ac349f845545c6adeaceb5585482c65ac6ca756
Amin Shayan <amin@shayan.net>
2018-10-15 14:37:12 +0330

    source ip field
    filter should read source ip field from logstash config

--- lib/logstash/filters/ip2location.rb
+++ lib/logstash/filters/ip2location.rb
@@ -46,7 +46,7 @@ class LogStash::Filters::IP2Location < LogStash::Filters::Base
   public
   def filter(event)
     json = JSON.parse(event.to_json)
-    ip = json["clientip"]
+    ip = json[@source]
 
     return unless filter?(event)
     if value = Cache.find(event, ip, @ip2locationfilter, @cache_size).get('ip2location')
