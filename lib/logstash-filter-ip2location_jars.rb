4fddc87df4f58bf47691298b7235c3b48c648743
ip2location <support@ip2location.com>
2018-10-16 14:12:12 +0800

    Fixes for sourceip field

--- lib/logstash-filter-ip2location_jars.rb
+++ lib/logstash-filter-ip2location_jars.rb
@@ -1,3 +1,3 @@
 require 'jar_dependencies'
 require_jar('com.ip2location.ip2location', 'ip2location', '8.0.3')
-require_jar('org.logstash.filters', 'logstash-filter-ip2location', '2.0.1')
+require_jar('org.logstash.filters', 'logstash-filter-ip2location', '2.0.2')
